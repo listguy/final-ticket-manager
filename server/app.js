efc6efd6648eb47d9c4ec2fd8874a4c7a173c0d5
listguy <nitzanjl@gmail.com>
2020-08-24 12:57:35 +0300

    2/3 server tests pass. server done for now

--- server/app.js
+++ server/app.js
@@ -1,5 +1,77 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const fs = require("fs").promises;
 
+const dbPath = "./data.json";
+app.use(express.json());
+app.get("/api/tickets", async (req, res) => {
+  const data = await fs.readFile(dbPath);
+  const filterParam = req.query.searchText;
+
+  try {
+    const json = JSON.parse(data);
+    if (filterParam) {
+      const filterRegx = new RegExp(`${filterParam}`, "i", "g");
+      filteredData = Array.from(json).filter((elem) =>
+        filterRegx.test(elem.title)
+      );
+      res.send(filteredData);
+    } else {
+      res.send(json);
+    }
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+app.post("/api/tickets/:ticketId/done", async (req, res) => {
+  const ticketId = req.params.ticketId;
+
+  const data = await fs.readFile(dbPath);
+  const json = JSON.parse(data);
+
+  for (let elem of json) {
+    if (elem.id === ticketId) {
+      elem.done = true;
+      break;
+    }
+  }
+
+  await fs.writeFile(dbPath, JSON.stringify(json));
+
+  res.send({ updated: true });
+});
+
+app.post("/api/tickets/:ticketId/undone", async (req, res) => {
+  const ticketId = req.params.ticketId;
+
+  const data = await fs.readFile(dbPath);
+  const json = JSON.parse(data);
+
+  for (let elem of json) {
+    if (elem.id === ticketId) {
+      elem.done = false;
+      break;
+    }
+  }
+
+  await fs.writeFile(dbPath, JSON.stringify(json));
+
+  res.send({ updated: true });
+});
+
+const setIfDone = async (ticketId, isDone) => {
+  const data = await fs.readFile("./data.json");
+  const json = JSON.parse(data);
+
+  for (elem of json) {
+    if (elem.id === ticketId) {
+      elem.done = isDone;
+      return;
+    }
+  }
+
+  await fs.writeFile("/data.json", JSON.stringify(json));
+};
 
 module.exports = app;
