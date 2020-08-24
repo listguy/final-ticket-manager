aa64658232f353cd25e8322cf48d2f50b3890f4e
listguy <nitzanjl@gmail.com>
2020-08-24 10:27:06 +0300

    set-up complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import Ticket from './components/Ticket'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+// import Ticket from './components/Ticket'
+import "./App.css";
 
 function App() {
-  return (
-    <main>
-    </main>
-  );
+  return <main>Check</main>;
 }
 
 export default App;
