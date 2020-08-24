a89c2df043d923fa376475eed0b4f959d48aeb2c
listguy <nitzanjl@gmail.com>
2020-08-24 19:55:16 +0300

    All tests pass

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,59 @@ import "./App.css";
 
 function App() {
   const [tickets, setTickets] = useState();
+  const [searchText, setSearchText] = useState();
+  // const [ticketsVisibility, setTicketsVisibility] = useState();
+  const [hiddenTickets, setHiddenTickets] = useState([]);
 
-  useEffect(() => {
-    const fetchTickets = async () => {
-      const { data } = await axios.get("/api/tickets");
-      setTickets(data);
-      console.log("Set");
-    };
-
-    fetchTickets();
-  }, []);
-
-  const printTickets = () => {
-    return tickets.map((t, i) => <Ticket key={`ticket${i}`} ticketData={t} />);
+  const fetchTickets = async () => {
+    let url = `/api/tickets${searchText ? `?searchText=${searchText}` : ``}`;
+    const { data } = await axios.get(url);
+    setTickets(data);
   };
 
-  return <main>{tickets && printTickets()}</main>;
+  useEffect(() => {
+    fetchTickets();
+  }, [searchText]);
+
+  const hideTicket = (id) => {
+    let updatedArr = hiddenTickets.slice();
+    updatedArr.push(id);
+    setHiddenTickets(updatedArr);
+  };
+
+  const printTickets = () => {
+    return tickets.map((t, i) => (
+      <Ticket
+        key={`ticket${i}`}
+        ticketData={t}
+        hide={hideTicket}
+        hidden={hiddenTickets.includes(t.id)}
+      />
+    ));
+  };
+
+  return (
+    <>
+      <div className="head">
+        <input
+          id="searchInput"
+          onChange={(event) => setSearchText(event.target.value)}
+        ></input>
+        {searchText && `Showing ${tickets.length} results`}
+        {hiddenTickets[0] && (
+          <div className="hidden-tickets-msg">
+            (<span id="hideTicketsCounter">{hiddenTickets.length}</span>
+            tickets hidden -
+            <span id="restoreHideTickets" onClick={() => setHiddenTickets([])}>
+              restore
+            </span>{" "}
+            )
+          </div>
+        )}
+      </div>
+      <main>{tickets && printTickets()}</main>
+    </>
+  );
 }
 
 export default App;
--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,22 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Ticket.css";
 
 function Ticket(props) {
-  const { ticketData } = props;
+  const { ticketData, hide, hidden } = props;
 
-  //   const makeTicketPart = (key, value) => {
-  //     if (key === "id") return;
-  //     return <div className={`ticket-${key}`}>{value}</div>;
-  //   };
-
-  return (
-    // <div className="ticket">
-    //   {Object.entries(ticketData).map((entry) => {
-    //     let [key, value] = entry;
-    //     return makeTicketPart(key, value);
-    //   })}
-    // </div>
+  return hidden ? null : (
     <div className="ticket">
+      <div className="hideTicketButton" onClick={() => hide(ticketData.id)}>
+        Hide
+      </div>
       <div className="ticket-title">{ticketData.title}</div>
       <div className="ticket-content">{ticketData.content}</div>
       <div className="ticket-footer">
@@ -29,7 +21,7 @@ function Ticket(props) {
         {ticketData.labels && (
           <div className="lables">
             {ticketData.labels.map((label) => (
-              <span className="lable">{label}</span>
+              <span className="label">{label}</span>
             ))}
           </div>
         )}
