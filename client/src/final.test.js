76c2dac2c28596b753eed7aa5f8d784e7a1a900e
listguy <nitzanjl@gmail.com>
2020-08-27 13:09:43 +0300

    removed headless:false

--- client/src/final.test.js
+++ client/src/final.test.js
@@ -42,9 +42,7 @@ jest.setTimeout(30000);
 const projectName = "1.Ticket Manager UI";
 describe(projectName, () => {
   beforeAll(async () => {
-    browser = await puppeteer.launch({
-      headless: false,
-    });
+    browser = await puppeteer.launch();
     page = await browser.newPage();
     useNock(page, ["http://localhost:3000/api"]);
 
