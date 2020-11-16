66d3ab2d5862899296ae04923c2b9246b1eace19
Nitzan Listman <67858069+listguy@users.noreply.github.com>
2020-11-16 21:38:18 +0200

    Update app.js
    potential bug fix

--- server/app.js
+++ server/app.js
@@ -8,9 +8,9 @@ app.use(express.json());
 
 app.use(express.static("../client/build"));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 app.get("/api/tickets/labels", async (req, res) => {
   /* An endpiont to get all labels.
